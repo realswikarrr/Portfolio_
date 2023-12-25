e0d3542cd7135ff8cb76133b020b916d87b5db93
Swikar Adhikari <swikara082@gmail.com>
2023-12-25 12:21:47 +1030

    fix: made navbar sticky

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,13 +5,15 @@ import { MdOutlineEmail } from "react-icons/md";
 
 export default function NavBar() {
   return (
-    <div className="mt-5 border border-t-0 border-r-0 border-l-0 border-b-[#c8c8dc]/20">
+    <div className="mt-5 border border-t-0 border-r-0 border-l-0 sticky top-0 border-b-[#c8c8dc]/20">
       <div className=" ">
         <nav className="flex justify-between px-2 py-2 items-center">
           <div className="flex space-x-3 justify-center items-center">
-            <h1 className="bg-black/10 dark:bg-[#c8c8dc]/10 cursor-pointer px-4 py-2 text-sm rounded-md text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-75">
-              Home
-            </h1>
+            <Link href="/">
+              <h1 className="bg-black/10 dark:bg-[#c8c8dc]/10 cursor-pointer px-4 py-2 text-sm rounded-md text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-75">
+                Home
+              </h1>
+            </Link>
           </div>
           <div className="flex space-x-3 text-2xl cursor-pointer">
             <Link href="https://github.com/realswikarrr" target="_">
